de1a85019167a7d480222a1fc184ef7c6fcc1535
efra <eliassaldarriaga65@outlook.es>
2024-04-14 03:51:48 -0500

    Agregando archivo de colores personalizado

---
+++ lib/presentation/constants/custom_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // static const Color primaryColor = Color.fromRGBO(187, 222, 251, 100);
+  static Color primaryColor = Colors.teal.shade100;
+  static Color appBarColor = Colors.orange.shade600;
+  static Color backgroundColor = Colors.orange.shade200;
+  static Color alterColor = Colors.teal.shade50;
+  static const Color textColor = Colors.black;
+  static const Color buttonColor = Colors.blue;
+  static Color floatingActionButton = Colors.orange.shade700;
+}
