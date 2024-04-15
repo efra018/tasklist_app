5cc180329949a586eab25d6a432fe001c4daeb08
efra <eliassaldarriaga65@outlook.es>
2024-04-15 04:10:00 -0500

    Modificando colores

--- lib/presentation/constants/custom_colors.dart
+++ lib/presentation/constants/custom_colors.dart
@@ -5,7 +5,7 @@ class AppColors {
   static Color primaryColor = Colors.teal.shade100;
   static Color appBarColor = Colors.orange.shade500;
   static Color backgroundColor = Colors.orange.shade200;
-  static Color alterColor = Colors.teal.shade50;
+  static Color alertColor = Colors.orange.shade200;
   static const Color textColor = Colors.black;
   static const Color buttonColor = Colors.blue;
   static Color floatingActionButton = Colors.orange.shade700;
